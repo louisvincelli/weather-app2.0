f24e06581eb47f3d7271861e4ca5b8579d242898
Louis Vincelli <louisvincelli@gmail.com>
2024-08-13 01:18:06 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/weather-app/",
+  base: "/weather-app2.0/",
   server:{
     port: 3000,
   }
